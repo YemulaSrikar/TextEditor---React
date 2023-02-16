74c5e900648eb89560c99c00347232b745e5b653
YemulaSrikar <104422175+YemulaSrikar@users.noreply.github.com>
2023-02-16 23:01:47 +0530

    Update styledComponents.js

--- src/components/TextEditor/styledComponents.js
+++ src/components/TextEditor/styledComponents.js
@@ -9,15 +9,30 @@ export const TextArea = styled.textarea`
   font-weight: ${props => (props.isClickedBold ? 'bold' : 'normal')};
   font-style: ${props => (props.isClickedItalic ? 'italic' : 'normal')};
   text-decoration: ${props =>
-    props.isClickedUnderline ? 'underline' : 'none'};
+    props.isClickedUnderline ? 'underline' : 'normal'};
   flex-wrap: wrap;
 `
-export const BoldIcon = styled.i`
+export const BoldButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
   color: ${props => (props.isClickedBold ? '#faff00' : '#f1f5f9')};
+  margin-top: 0px;
 `
-export const ItalicIcon = styled.i`
+export const ItalicButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
   color: ${props => (props.isClickedItalic ? '#faff00' : '#f1f5f9')};
+  margin-top: 0px;
 `
-export const UnderlineIcon = styled.i`
+export const UnderlineButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  outline: none;
   color: ${props => (props.isClickedUnderline ? '#faff00' : '#f1f5f9')};
+  margin-top: 0px;
 `
